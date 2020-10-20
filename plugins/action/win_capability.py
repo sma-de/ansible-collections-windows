889e10b2de50b9ef59ed623356c8cb92190f4ba7
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2020-10-20 17:50:33 +0200

    merged feature: added new action and role for ...
    ... managing windows capabilities
    
    Squashed commit of the following:
    
    commit 15c0b00c653b271ba379ad87305209f99365195b
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 14:44:08 2020 +0200
    
        wincap role: enable cap removing
    
    commit 05df52f5a243b0a8d8a27683426d538bc9d4341a
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 14:20:08 2020 +0200
    
        wincap role: finish os reboot notifying
    
    commit 592fa1732cbe6dd5fbf533881f77f9a33d06de71
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:39:36 2020 +0200
    
        debug wincap role 08
    
    commit ff6ef779aba245a4f087f73791d2cafcc9a103b1
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:32:28 2020 +0200
    
        debug wincap role 07
    
    commit bb4ac107087347486ac81a75fd0aaf05251247cc
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:28:15 2020 +0200
    
        debug wincap role 06
    
    commit 34bd82868035981c21efe2e42a2e08b2216edb05
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:10:27 2020 +0200
    
        debug wincap role 05
    
    commit 31a94618992d026294d9c8ca410e3d4e01aac9a8
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:08:09 2020 +0200
    
        debug wincap role 04
    
    commit 97eefd26a9416561ef81e068875776a3b652b5b4
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 10:01:55 2020 +0200
    
        debug wincap role 03
    
    commit ce4be82c3e19d5f7b3ad44614914351901647cb5
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 09:59:14 2020 +0200
    
        debug wincap role 02
    
    commit fabba0c78953bafc94a039b74af93a4982170d92
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 09:56:12 2020 +0200
    
        debug wincap role
    
    commit 70bc8ef2b644432e56cee06876695035be8946e0
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 09:53:59 2020 +0200
    
        renamed jenkins slave role
    
    commit 5aa8462b63c06a8b2e668f50641fe7e9a4a83c6a
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 20 09:46:25 2020 +0200
    
        initted jenkins slave role for managing ...
    
        ... windows jenkins nodes
    
    commit fdc41cd209c3cebc23ca0f4e8a31a2d042f3afca
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 18:38:48 2020 +0200
    
        bugfixing new wincap module 06
    
    commit 4b5464095a7fdccc5bed280e9e5423b0c7d9e86a
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 18:32:49 2020 +0200
    
        bugfixing new wincap module 05
    
    commit c6986b495d18f7a04fe42ee1e718500fafe98d2c
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 15:14:49 2020 +0200
    
        bugfixing new wincap module 04
    
    commit 90fdd33c0f9f20f5a9110ac38b1f7067fb183592
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 14:59:22 2020 +0200
    
        bugfixing new wincap module 03
    
    commit 11ac23d89fbd37939948cb10b88179817bef583e
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 14:53:58 2020 +0200
    
        bugfixing new wincap module 02
    
    commit 143771cbb2645ed81218f8364fcbd25ceebeaa6d
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 14:49:51 2020 +0200
    
        bugfixing new wincap module
    
    commit 039037b3d2600396b3140b608db4838f49465b07
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Mon Oct 19 14:42:46 2020 +0200
    
        added new action / module for managing windows ...
    
        ... capabilities

---
+++ plugins/action/win_capability.py
@@ -0,0 +1,211 @@
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+import collections
+import re
+
+from ansible.errors import AnsibleOptionsError, AnsibleError ##, AnsibleModuleError##, AnsibleError
+####from ansible.module_utils._text import to_native
+from ansible.module_utils.six import iteritems, string_types
+
+from ansible_collections.smabot.windows.plugins.module_utils.plugins.action_base_win import ActionBaseWin
+##from ansible_collections.smabot.windows.plugins.module_utils.utils.powershell import to_pshell_array_param
+from ansible_collections.smabot.base.plugins.module_utils.utils.utils import ansible_assert
+
+
+CAP_STATES = {
+  4: 'INSTALLED',
+}
+
+
+class ActionModule(ActionBaseWin):
+
+    def __init__(self, *args, **kwargs):
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self._supports_check_mode = False
+        self._supports_async = False
+
+
+    @property
+    def argspec(self):
+        tmp = super(ActionModule, self).argspec
+
+        tmp.update({
+          'caps': (list(string_types) + [[dict] + list(string_types)]),
+          'state': (list(string_types), 'present', ['present', 'absent']),
+        })
+
+        return tmp
+ 
+
+    def _on_getcap_error(self, modres):
+        state = self.get_taskparam('state')
+
+        if state == 'absent':
+            # nothing to do when state is absent
+            return False
+
+        err = modres['stderr']
+
+        tmp = re.search(
+          r'(?i)(fehlercode|errorcode):\s+(0x[a-fA-F0-9]+)', err
+        )
+
+        if tmp:
+            ## set a special more meaningful error msg for specific errrors
+            tmp = int(tmp.group(2), 16)
+
+            ## somewhat hard to find, but it seems possible to disable 
+            ## machine local installation of windows capabilities by domain 
+            ## policies or similar, which probably make sense, which than 
+            ## results in this non-saying error code when calling "Get-Caps"
+            if tmp == 0x8024002e:
+                modres['msg'] = "Could not query external windows"\
+                   " capability sources, maybe installation of capabilities"\
+                   " is disabled by administrator or domain policy"
+                return False
+
+        return False
+
+
+    def run_specific(self, result):
+        caps = self.get_taskparam('caps')
+        state = self.get_taskparam('state')
+
+        ## normalize caps arg
+        if not isinstance(caps, list):
+            ## assume single string
+            caps = [caps]
+
+        tmp = []
+
+        for c in caps:
+            if not isinstance(c, collections.abc.Mapping):
+                ## assume single string which represents name (plus optional meta)
+                c = c.split('~')
+
+                nc = { 'name': c[0] }
+
+                if len(c) > 1:
+                    nc.update(locale=c[3], version=c[4])
+
+                c = nc
+
+            if not c.get('version', False) and state == 'present':
+                raise AnsibleOptionsError(
+                   "Bad param '{}': when installing capabilities"\
+                   " (state == present) a version must be"\
+                   " specified".format(c['name'])
+                )
+
+            tmp.append(c)
+
+        caps = tmp
+
+        cap_info = {}
+        restart_needed = False
+
+        for c in caps:
+            cap_name = [c['name']]
+            name_suffix = []
+
+            if 'locale' in c:
+                name_suffix.append(c['locale'])
+
+            if 'version' in c:
+                name_suffix.append(c['version'])
+
+            if name_suffix:
+                name_suffix = ['', ''] + name_suffix
+
+            cap_name = '~'.join(cap_name + name_suffix)
+
+            ##
+            ## check if caps are already installed
+            ##
+            cmd = ['Get-WindowsCapability', '-Online']
+
+            if state == 'absent':
+                ##
+                ## note: LimitAccess is an important parameter here, because 
+                ##   it possible that because of AD/Domain restrictions 
+                ##   installation of capabilities is forbidden, but we can 
+                ##   still use this module for removing capabilities, but the 
+                ##   thing is that normally just listing caps with "Get-Cap" 
+                ##   already errors out when installing is forbidden, as this 
+                ##   on default already tries to query online resources for 
+                ##   caps, but with the access limit param we actually only 
+                ##   query the local machine, which should always be fine
+                ##
+                cmd += ['-LimitAccess', '| where state -eq "Installed"']
+
+            cmd += ['| where Name -Match "{}"'.format(cap_name)]
+
+            tmp = self.exec_powershell_script(' '.join(cmd), 
+               data_return=True, on_error=self._on_getcap_error
+            )
+
+            tmp = tmp['result_json']
+
+            if not tmp:
+                if state == 'absent':
+                    ## caps we want to remove are not installed, noop
+                    continue
+
+                raise AnsibleError(
+                   "Failed to install capability for given name '{}':"\
+                   " could not match name to capability".format(cap_name)
+                )
+
+            ## found installed caps
+            if not isinstance(tmp, list):
+                ## possible that we find one ore multiple 
+                ## matching caps, unify both cases
+                tmp = [tmp]
+
+            for ec in tmp:
+                cap_info[ec['Name']] = { 'details': ec }
+
+            if state == 'present':
+                if len(tmp) > 1:
+                    raise AnsibleOptionsError(
+                       "Given caps name '{}' is ambigious (matches more"\
+                       " than one capability). For installing caps"\
+                       " (state == present) name must match exactly one"\
+                       " capability.".format(cap_name)
+                    )
+
+                new_caps = []
+
+                ## filter out already installed caps
+                for fc in tmp:
+                    if CAP_STATES.get(fc['state'], 'UNKNOWN') != 'INSTALLED':
+                        new_caps.append(fc)
+
+                if not new_caps:
+                    ## caps we want to install already there, noop
+                    continue
+
+                ## install new caps
+                result['changed'] = True
+                ansible_assert(False, "TODO: handle installing new caps")
+                continue
+
+            ## remove existing caps
+            result['changed'] = True
+
+            for ec in tmp:
+                cmd = 'Remove-WindowsCapability -Online -Name "{}"'.format(ec['Name'])
+                rmres = self.exec_powershell_script(cmd, data_return=True)
+                rmres = rmres['result_json']
+
+                cap_info[ec['Name']]['remove'] = rmres
+
+                restart_needed = restart_needed or rmres['RestartNeeded']
+
+        result['capabilities'] = cap_info
+        result['restart_needed'] = restart_needed
+        return result 
+
